7bfe9baec6b796c21470b5ecb62ec852818906c5
Lyons800 <94101691+Lyons800@users.noreply.github.com>
2026-05-31 19:38:34 +0100

    chore(cask): set v1.0.0 sha256; require Apple Silicon

--- homebrew-murmur/Casks/murmur.rb
+++ homebrew-murmur/Casks/murmur.rb
@@ -1,6 +1,6 @@
 cask "murmur" do
   version "1.0.0"
-  sha256 "REPLACE_WITH_DMG_SHA256"
+  sha256 "9859be76f35e4a88c316d5792d873273dcaa0f2757127069f271ee406f2f445e"
 
   url "https://github.com/Lyons800/murmur/releases/download/v#{version}/Murmur.dmg"
   name "Murmur"
@@ -8,6 +8,7 @@ cask "murmur" do
   homepage "https://murmur.dev"
 
   depends_on macos: ">= :sonoma"
+  depends_on arch: :arm64
 
   app "Murmur.app"
 
